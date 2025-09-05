eb79f580a9e16d8340681f25c2e8649aae218ba5
AGH-0x01 <gearheadauto9@gmail.com>
2025-09-05 17:26:11 +0100

    Update tg-bot.py
    Bot description added

--- tg-bot.py
+++ tg-bot.py
@@ -1,1 +1,9 @@
+"""
+CTF Telegram Bot — CtffTest_bot 
+========================================================
 
+OVERVIEW
+- The bot never discloses flags.
+- It returns the a response irrespective of the message input. However, the bot should not reveal any information related to the attacker unless the attacker proves their identity to the bot.
+
+""""
